bd4c785fd100fdb8aca1ac178a04e77d6b3fed29
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-19 23:18:13 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+/ Створюємо шаблонний рядочок!
+const galleryOfImg = ({ preview, original, description }) =>
+    `    
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+// Створюємо акумулятор
+const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
+//Шукаємо  class = gallery
+const galleryList = document.querySelector('.gallery');
+//Додаємо шаблоні рядки
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+//Делегування кліків
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onclick);
+
+function onclick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+    const modal = basicLightbox.create(`
+            <img src="${event.target.dataset.source}" width="800" height="600">
+        `).show()
+
+    document.addEventListener("keydown", onEscClick);
+    function onEscClick(event) {
+        if (event.code === "Escape") {
+            modal.close();
+            document.removeEventListener("keydown", onEscClick);
+        }
+    }
+};
+
