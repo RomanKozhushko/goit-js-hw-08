26b2628da15db34d4076a52b8ff6f888c59bdf9d
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-20 16:40:49 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,13 @@
 
+    const iframe = document.querySelector('iframe');
+      const player = new Vimeo.Player(iframe);
 
+      player.on('timeupdate', function (timeupdate) {
+        const timeControl = timeupdate;
+
+        console.log(timeControl );
+      });
+
+        player.getVideoTitle().then(function (title) {
+        console.log('title:', title);
+      });
