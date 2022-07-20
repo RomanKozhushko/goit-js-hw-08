43b4ab90bd0cd8bd21c2a38f8e364e9f4069562d
RomanKozhushko <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-20 19:39:32 +0000

    Deploying to gh-pages from @ RomanKozhushko/goit-js-hw-08@1fe13f995292b48c6864e9e7e9e51cd6eaedafcc 🚀

--- 02-video.042474e7.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){let t=e;localStorage.setItem("LOCALSTORAGE_KEY",JSON.stringify(t))}));const n=localStorage.getItem("LOCALSTORAGE_KEY"),o=JSON.parse(n).seconds;t.setCurrentTime(o).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.042474e7.js.map
