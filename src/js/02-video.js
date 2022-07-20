1fe13f995292b48c6864e9e7e9e51cd6eaedafcc
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-20 22:39:02 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,26 @@
+import throttle from "lodash.throttle";
 
       const iframe = document.querySelector('iframe');
       const player = new Vimeo.Player(iframe);
       const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
+
       player.on('timeupdate', function (timeupdate) {
           let timeControl = timeupdate;
         localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(timeControl));
-        
       });      
-  
         const timePoint = localStorage.getItem('LOCALSTORAGE_KEY')
         const timeReload = JSON.parse(timePoint);
           
 const secondOfReload = timeReload.seconds;
         
-player.setCurrentTime(secondOfReload).then(function(seconds) {
+player.setCurrentTime(secondOfReload).then(function(secondOfReload) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
             break;
-
         default:
             // some other error occurred
             break;
