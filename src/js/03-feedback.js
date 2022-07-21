cfe0025620583950e8d29328b5bbf6863cb285fc
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-21 09:39:25 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+
+//Під час відправлення форми сторінка не повинна перезавантажуватися.
+function onFormSubmit(event) {
+    event.preventDefault();
+
+ //Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.       
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+    });  
+//Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, 
+//а значення поля - значенням властивості.Для доступу до елементів форми використовуй властивість elements.
+    let user = {
+        email: formElements.email.value,
+        password: formElements.password.value,
+    };
+  //Вивести в Consol  +  RESET   
+    console.log(user);
+    document.querySelector('form').reset();
+  }
+    
