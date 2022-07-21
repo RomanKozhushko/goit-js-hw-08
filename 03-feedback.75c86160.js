d74db6857dc5e3b25557cac66936e26fd54280fc
RomanKozhushko <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-21 06:39:51 +0000

    Deploying to gh-pages from @ RomanKozhushko/goit-js-hw-08@cfe0025620583950e8d29328b5bbf6863cb285fc 🚀

---
+++ 03-feedback.75c86160.js
@@ -0,0 +1,2 @@
+document.querySelector(".login-form").addEventListener("submit",(function(e){e.preventDefault(),new FormData(e.currentTarget).forEach((function(e,r){}));var r={email:formElements.email.value,password:formElements.password.value};console.log(r),document.querySelector("form").reset()}));
+//# sourceMappingURL=03-feedback.75c86160.js.map
---
+++ 03-feedback.e2b7af03.js
@@ -0,0 +1,2 @@
+document.querySelector(".login-form").addEventListener("submit",(function(e){e.preventDefault();new FormData(e.currentTarget).forEach(((e,r)=>{}));let r={email:formElements.email.value,password:formElements.password.value};console.log(r),document.querySelector("form").reset()}));
+//# sourceMappingURL=03-feedback.e2b7af03.js.map
