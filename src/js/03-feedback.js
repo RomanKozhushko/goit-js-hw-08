4f932f16d67a968153b7a7417c95aa701725f27b
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-21 19:03:10 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,21 +4,23 @@ const LOCALSTORAGE_KEY = "feedback-form-state";
 
 // throttle(inputItem, 500)
 //1.Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message. 
-form.addEventListener("input", inputItem);  
+form.addEventListener("input", throttle(inputItem, 500));  
 function inputItem(event) {
   event.preventDefault();
   const email = event.currentTarget.email.value;
   const message = event.currentTarget.message.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ email, message }));
-  // console.log(email);
+  console.log(email);
 }
 window.addEventListener('DOMContentLoaded', updateOutput);
 
-  function updateOutput(event) {
-    form.elements.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
-    form.elements.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
-
+function updateOutput() {
+  const parseMail = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
+  const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
+    email.value = parseMail;
+    message.value = parseMessage;
 };
+
  // 3. Під час сабміту форми очищуй сховище і поля форми,
  // а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
 form.addEventListener("submit", reset);  
@@ -32,48 +34,5 @@ form.addEventListener("submit", reset);
     console.log(itemArr);
     form.reset();
     localStorage.clear();
+   
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// form.addEventListener("submit", saveItem);  
-// function saveItem(event) {
-//   event.preventDefault();
-//   const email = event.currentTarget.email.value;
-//   const message = event.currentTarget.message.value;
-//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ email, message }));
-//   form.reset();
-// }
-// updateOutput();   
-// window.addEventListener('DOMContentLoaded', updateOutput);
-// function updateOutput(event) {
-//   form.elements.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
-//   form.elements.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
-// }
