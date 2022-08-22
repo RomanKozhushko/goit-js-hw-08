03422b164d85d8d658a70437579b5f729f14ecc4
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-22 10:36:17 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
